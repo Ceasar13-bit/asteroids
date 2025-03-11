ce9c4642c21b4a7e8db305a0a7116933b14a7c4f
Ceasar13-bit <cezary.gajewski13@gmail.com>
2025-03-11 20:07:09 +0100

    Add asteroids

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+import sys
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid = AsteroidField()  
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     fps = pygame.time.Clock()
     dt = 0
@@ -15,12 +22,16 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        \
+          
         updatable.update(dt)
 
         screen.fill((0,0,0))
         for thing in drawable:
             thing.draw(screen)
+        
+        for thing in asteroids:
+            if player.collision(thing):
+                sys.exit("Game Over")
             
         pygame.display.flip()
         dt = fps.tick(60) / 1000
